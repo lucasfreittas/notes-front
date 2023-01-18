b00d3ab2ebd44f171bb61bf82e963a290a65e34f
Lucas <l.freittas@outlook.com>
2023-01-18 17:20:53 -0300

    Home Page Ok

--- RocketNotes/src/pages/Home/index.jsx
+++ RocketNotes/src/pages/Home/index.jsx
@@ -5,6 +5,8 @@ import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
 import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
+import { Note } from '../../components/Note';
 
 export function Home(){
     return(
@@ -27,7 +29,16 @@ export function Home(){
             </Search>
 
             <Content>
-
+                <Section title='Minhas Notas'>
+                    <Note data={{
+                        title: 'React',
+                        tags:[
+                            {id: '1', name: 'react'},
+                            {id: '2', name:'rocketseat'}
+                        ]
+                    }}
+                    />
+                </Section>
             </Content>
 
             <NewNote>
